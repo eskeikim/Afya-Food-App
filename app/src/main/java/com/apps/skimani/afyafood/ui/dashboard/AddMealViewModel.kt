aec82dc519229a016b2003ed0afae3896d86dff6
eskei <skimani@ekenya.co.ke>
2021-06-20 18:46:07 +0300

    First initialization

---
+++ app/src/main/java/com/apps/skimani/afyafood/ui/dashboard/AddMealViewModel.kt
@@ -0,0 +1,13 @@
+package com.apps.skimani.afyafood.ui.dashboard
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class AddMealViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is dashboard Fragment"
+    }
+    val text: LiveData<String> = _text
+}
